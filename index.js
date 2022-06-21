a6062614f9516c6b430ff627e46f314d034688a4
Onyekachi Fakoya <88961671+kachifako@users.noreply.github.com>
2022-06-21 15:04:32 +0100

    commit on master

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let aboutMe = {
+    name: "Onyekachi Fakoya",
+    height : "5ft 5inches",
+    country: "Nigeria"
+}
+
+document.getElementById("stats").innerHTML = "Name: " + aboutMe.name + ", " + "Height: " + aboutMe.height + ", " + "Country: " + aboutMe.country
+
+alert('Name: '  + aboutMe.name + ", " + 'Height: '  + aboutMe.height + ", " + 'Country: ' + aboutMe.country)
+
+console.log(aboutMe)
